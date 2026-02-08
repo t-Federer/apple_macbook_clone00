c42fc8c428dc1292f79585a5aa7c02c499f796cc
Federico <talaricofederico@outlook.com>
2026-02-08 03:43:36 +0100

    Safe-public-folder test completed (#1)
    * chore(gitignore): exclude local-assets and prepare safe public folder
    
    * chore: complete local-assets test. Ready to restore standard public folder usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App() {
         return (
-                <h1 className="text-3xl font-bold underline text-blue-500">
-                        Hello world!
-                </h1>
+                <div>
+                        <h1>Test asset from local-assets</h1>
+                        <p>Is the image showing up?</p>
+                        <img
+                                src="/local-assets/battery.png"
+                                alt="Test"
+                                style={{ width: "100px", borderRadius: "8px", marginTop: "1rem" }}
+                        />
+                </div>
         )
 }
 

a1b9e144add51f2a9e14c778f6160af77598046f
t-Federer <talaricofederico@outlook.com>
2026-02-09 02:05:58 +0100

    feat: add ProductViewer component with controls and Canvas to display 3D models. Installed Three, drei fiber.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import ProductViewer from "./components/ProductViewer"
 
 function App() {
         return (
                 <main>
                         <NavBar />
                         <Hero />
+                        <ProductViewer />
                 </main>
         )
 }

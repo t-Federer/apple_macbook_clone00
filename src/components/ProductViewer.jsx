c3da5e9efa20cbcc2eba26f6bc88548289ac6d51
t-Federer <talaricofederico@outlook.com>
2026-02-09 02:27:13 +0100

    feat(canvas): add 3D models. Use npx gltfjsx model.glb -T to convert the models into react components.

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = () => {
         const { color, scale, setColor, setScale } = useMacbookStore();
@@ -40,7 +41,8 @@ const ProductViewer = () => {
                         </div>
 
                         <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-                                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+                                <ambientLight intensity={5}/>
+                                <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
                                 <OrbitControls enableZoom={false} />
                         </Canvas>
                 </section>

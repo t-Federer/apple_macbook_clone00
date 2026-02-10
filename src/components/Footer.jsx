882feffbe0d32b9a870739e97160f4f64293d968
t-Federer <talaricofederico@outlook.com>
2026-02-10 01:27:23 +0100

    feat: implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
+import { footerLinks } from "../constants"
+
 const Footer = () => {
         return (
                 <footer id="footer">
-                        Footer
+                        <div className="info">
+                                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+                                <img src="/logo.svg" alt ="Apple logo"/>
+                        </div>
+
+                        <hr />
+
+                        <div className="links">
+                                <p>Copyright © 2026 Apple Inc. All rights reserverd.</p>
+                                <ul>
+                                        {footerLinks.map(({label, link}) => (
+                                                <li key={label}>
+                                                        <a href={link}>{label}</a>
+                                                </li>
+                                        ))}
+                                </ul>
+                        </div>
                 </footer>
         )
 }

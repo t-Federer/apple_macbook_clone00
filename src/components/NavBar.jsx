cbe150c1bdb502efb10c54b7cbbf9d2ea63dfc99
t-Federer <talaricofederico@outlook.com>
2026-02-24 04:40:44 +0100

    fix: NavBar links now navigate to #

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                                 <ul>
                                         {navLinks.map(({label}) => (
                                                 <li key={label}>
-                                                        <a href={label}>{label}</a>
+                                                        <a href="#">{label}</a>
                                                 </li>
                                         ))}
                                 </ul>
